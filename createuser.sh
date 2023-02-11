e29beb0af550f6d2f8d58815f502591ab046f3c2
Abhishek Singh <abhishek.abvp.as@gmail.com>
2023-02-11 20:08:18 +0530

    env username changed to user

--- createuser.sh
+++ createuser.sh
@@ -8,22 +8,22 @@ while true; do
   PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
   # Generate a random username
-  USERNAME=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
+  USER=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
 
   # Create the user
-  useradd $USERNAME
-  echo "$PASSWORD" | passwd --stdin $USERNAME
+  useradd $USER
+  echo "$PASSWORD" | passwd --stdin $USER
 
   # Add the user to the sudoers file
-  echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers
+  echo "$USER ALL=(ALL) ALL" >> /etc/sudoers
 
   # Send Slack message with username and password
-  SLACK_MESSAGE="Username: $USERNAME\nPassword: $PASSWORD"
+  SLACK_MESSAGE="Username: $USER\nPassword: $PASSWORD"
   curl -X POST -H 'Content-type: application/json' --data "{'text': '$SLACK_MESSAGE'}" $SLACK_WEBHOOK_URL
 
   # Sleep for 3 days
   sleep 259200
 
   # Delete the user
-  userdel $USERNAME
+  userdel $USER
 done
